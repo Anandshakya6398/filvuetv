a967991713bf64d58f5df9a61bcdfded0937805a
anandshakya6398 <prabhatshakya166@gmail.com>
2025-07-05 07:31:46 +0530

    some updation

--- app/(auth)/page.jsx
+++ app/(auth)/page.jsx
@@ -1,9 +1,12 @@
+import { Button } from '@/components/ui/button';
 import React from 'react'
 
 const AboutPage = () => {
   return (
     <div>
       this is the about page
+      <Button >Primary Button</Button>
+      <Button variant="Primary">Secondary Button</Button>
     </div>
   )
 }
---
+++ app/watchlist/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      watchlist page
+    </div>
+  )
+}
+
+export default page
